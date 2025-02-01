4dceeaa89ed14eb8c63e5e7404a892f9a147423e
Songyuan Liu <lsystevee@gmail.com>
2025-02-01 15:47:59 -0500

    Initial commit

---
+++ ARTestTests/ARTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARTestTests.swift
+//  ARTestTests
+//
+//  Created by Songyuan Liu on 1/30/25.
+//
+
+import Testing
+@testable import ARTest
+
+struct ARTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
